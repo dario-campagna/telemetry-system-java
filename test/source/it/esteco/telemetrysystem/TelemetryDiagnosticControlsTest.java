69f0189ce732278c935d585ac658be170ce62aa0
Dario Campagna <campagna@esteco.com>
2017-03-06 09:47:46 +0100

    Exercise environment ready

---
+++ test/source/it/esteco/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -0,0 +1,10 @@
+package it.esteco.telemetrysystem;
+
+import org.junit.Test;
+
+public class TelemetryDiagnosticControlsTest {
+
+    @Test
+    public void sendDiagnosticMessageAndReceiveStatusResponse() throws Exception {
+    }
+}
